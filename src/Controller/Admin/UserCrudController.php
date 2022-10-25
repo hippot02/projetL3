b610d0a8159aef0181deb4a586cef899bf9b4858
LeBonYag <yagovolo@gmail.com>
2022-10-25 18:58:41 +0200

    Creation des Roles mais pas terminer

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -11,6 +17,18 @@ class UserCrudController extends AbstractCrudController
     {
         return User::class;
     }
+
+    public function configureFields(string $pageName) :iterable
+    {
+        return [
+            IntegerField::new( 'id') ,
+            TextField::new('email'),
+            ArrayField::new('roles'),
+
+        ];
+    }
+
+
 
     /*
     public function configureFields(string $pageName): iterable
